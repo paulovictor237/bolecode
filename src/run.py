ea893142b165cf58cdb7f2a05c2fb4343d46bcd2
Paulo Victor Duarte <paulovictor237@gmail.com>
2024-02-09 22:44:13 -0300

    assets

--- src/run.py
+++ src/run.py
@@ -1,3 +1,4 @@
+import json
 from PyPDF2 import PdfWriter
 import base64
 from PIL import Image
@@ -5,6 +6,8 @@ import io
 import barcode
 from barcode.writer import ImageWriter
 from weasyprint import HTML, CSS
+
+from data import format_data
 
 class PDFGenerator:
     def write_file(self, data, filename):
@@ -53,21 +56,22 @@ pdf = PDFGenerator()
 qr_code = "iVBORw0KGgoAAAANSUhEUgAAAPoAAAD6AQAAAACgl2eQAAACy0lEQVR4Xu2XS3LlIBAE4SJw/1v4KHARmMzG9vssHLOw2hthj54l0hHl7qpGU/bP66O8P3lbN3DWDZx1A2f9HzBKqXuu0lfrq/tZ+oyHecDke7S6Ope5WuncVh9mAt6OosDG5uZ21cOkAqNPRBYeRqFa/wMAgX2wU6obUbpcgG/80tzctm37Gc/TAP0539e7q9/3fxlwmRWqhVuiaxjnrCwAnyCpa1Z+3ESngLSAsgCMijrkrU/n6p5piPKAPZqqkNqH/7rt6+th2gRAkTu2SqNI1WrBvP4VVwPqWZEXumWJgLfpyQOWLql2B7HLXvlbx7hZAFmhTJwoXpA3h8l5alYCQEmwKY0C6GeKxgGTCRxd7Di7JCnc9DYRiKHFWcKlWSBEvoY3AdAy7GAagOIBZ2piqKQBhpUC2SuwUyxWKhDBjSvmVXCdNsr9NMBbSzMdI+00jXEaGc4CLEsNrzLDzE/8gtc8gPkZM2uq9mt6OFYSAdyqS7RKjRTHeJdOBNS2fdn1UPHlfyvyKbzXA7xsntPUo5W8EN64/RaZAKArXDJ0jbYxyi8D5HrA7kxPlUqz6FO4GNk1EbA1PF8O8DhdXWA9EVgIcmYso4PkuKo8E6gGltERh/vQOPulWQkAfUKkJSruBNWNbyJgTM1MlIp++R9Df+iJQAgrehWNbvHxOc3SAEQxMjRsCOwRoh6ezQN4GrMLt9Kv1U50n9J9PUBV4lAzPaWa4vh6NCsDwKHGBWlRJmkqVh/JSgCKscEpFoz0tDBOexJ5PYBAm7Q92hkjpkbBT+G9HnAZXvoT5eqENyZZIhCKhlZxjtO4Fu+969u0CYAWoUq4hGcRmKHIlgrYJRJLrZCrVeprs5IA4mta6RAV21G8h2mzAM+TM8y27iE67ucBwfBuE2cKep1hztVEwM4wR1FIv2Jw0LtupvOAn9YNnHUDZ93AWb8A/AN4HAw5z2fBbAAAAABJRU5ErkJggg=="
 base64_string = pdf.encode_decode(pdf.barcode_2_decode('5901234123457'))
 
+folder = 'src/assets/'
+
 # Ler o conteúdo do arquivo template.html
-with open('src/index.html', 'r') as file:
+with open(folder + 'index.html', 'r') as file:
     html_content = file.read()
     
-with open('src/logo.svg', 'rb') as file:
-    logo_content = file.read()
-    logo_base64 = base64.b64encode(logo_content).decode('utf-8')
-
-# Substituir as variáveis no conteúdo HTML
-html_content = html_content.replace("logo.svg", f"data:image/svg+xml;base64,{logo_base64}")
+with open(folder +  "data.json", 'r') as file:
+        json_data = json.load(file)
 
 # Substituir as variáveis no conteúdo HTML
 html_content = html_content.replace('{{ qr_code_base64 }}', qr_code)
 html_content = html_content.replace('{{ barcode_base64 }}', base64_string)
 
+html_content = format_data(html_content,json_data)
+
 # Convertendo o HTML para PDF com WeasyPrint
 html = HTML(string=html_content)
-html.write_pdf(target='bolecode.pdf', stylesheets=[CSS('src/styles.css')])
+html.write_pdf(target='bolecode.pdf', stylesheets=[CSS(folder + 'styles2.css')])
+# html.write_pdf(target='bolecode.pdf')
